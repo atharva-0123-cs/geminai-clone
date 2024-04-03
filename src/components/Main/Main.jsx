d0de2e0067d6ad83de1a282538a78279e5428365
atharva-0123-cs <atharva123king@gmail.com>
2024-04-03 21:30:07 +0530

    add e

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -20,7 +20,7 @@ export default function Main() {
   return (
     <div className="main">
       <div className="nav">
-        <p>Gemini</p>
+        <p>Geminie</p>
         <img src={assets.user_icon} alt="" />
       </div>
       <div className="main-container">
